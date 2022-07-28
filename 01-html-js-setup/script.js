f4bec6ff9e85b5dcb6f7038e6f8bcc5d376a596c
Jonas <jonas.leonhard@fork.de>
2022-07-28 16:42:55 +0200

    feat(html-js-setup): added simple html-js setup from Lession-03

---
+++ 01-html-js-setup/script.js
@@ -0,0 +1,28 @@
+const config = {
+  width: 800,
+  height: 600,
+};
+
+const scene = new THREE.Scene();
+
+// ? Camera
+const camera = new THREE.PerspectiveCamera(75, config.width / config.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// ? Box
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotation.y = 1;
+mesh.rotation.x = 0.5;
+scene.add(mesh);
+
+// ? Renderer
+const canvas = document.getElementById("webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(config.width, config.height);
+
+renderer.render(scene, camera);
