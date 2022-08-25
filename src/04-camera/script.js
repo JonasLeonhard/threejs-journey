0cbb6feb7ef60a21026aff85bafadd1316d41561
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-08-25 17:58:30 +0200

    feat(04-camera): added lesson 04
    - added lesson 04

---
+++ src/04-camera/script.js
@@ -0,0 +1,79 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+  near: 0.1,
+  far: 100,
+};
+
+//cursor
+const cursor = { x: 0, y: 0 };
+window.addEventListener("mousemove", (event) => {
+  console.log(event.clientX);
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  sizes.near,
+  sizes.far
+);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   sizes.near,
+//   sizes.far
+// );
+
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+// const clock = new THREE.Clock();
+
+const tick = () => {
+  // const elapsedTime = clock.getElapsedTime();
+  camera.position.x = cursor.x;
+  camera.position.y = cursor.y * -1;
+  // Update objects
+  // mesh.rotation.y = elapsedTime;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
